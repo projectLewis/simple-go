38bddab1ca3f939d9c3a4e28c5f5d7708401a26c
Seth Lewis <lewiscatenary@gmail.com>
2020-05-15 22:35:02 -0500

    initial server commit

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func printInitial(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("initial commit")
+}
+
+// PORT is the default port
+const PORT = "8080"
 
 func main() {
-	fmt.Println("initial commit")
+	r := mux.NewRouter()
+	r.HandleFunc("/", printInitial)
+	fmt.Println("Now listening on port: ", PORT)
+	log.Fatal(http.ListenAndServe(":"+ PORT, r))
 }
